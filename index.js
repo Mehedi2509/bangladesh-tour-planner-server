22f76fc836bcf01c71678a3095a675d06aadee46
Mahadi2509 <mahadi2509@gmail.com>
2021-10-30 20:48:33 +0600

    added tour  plans server

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zdula.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const database = client.db("bangladesh_tour_planner");
+        const tourPlansCollection = database.collection("tour_plan");
+
+        // Get tour plan
+        app.get('/tourPlans', async (req, res) => {
+            const query = {};
+            const cursor = tourPlansCollection.find(query);
+            const tourPlans = await cursor.toArray();
+            res.send(tourPlans);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Server Is Active')
+})
+
+app.listen(port, () => {
+    console.log('Server running is', port)
+})
