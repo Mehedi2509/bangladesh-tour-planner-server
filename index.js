8591b5f28627cc1cdd61da25b528f8885303e85a
Mahadi2509 <mahadi2509@gmail.com>
2021-10-31 21:26:02 +0600

    added single plan

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -26,6 +27,15 @@ const run = async () => {
             const tourPlans = await cursor.toArray();
             res.send(tourPlans);
         })
+
+        // Get single tour plan
+        app.get('/tourPlans/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await tourPlansCollection.findOne(query);
+            res.send(result);
+        })
+
     }
     finally {
         // await client.close();
